e39710dbee86e3d078d18026ccfec9ec45a25605
Aa <aa@example.com>
2025-05-04 11:40:42 +0300

    added tests

--- test/artist.e2e-spec.ts
+++ test/artist.e2e-spec.ts
@@ -4,6 +4,12 @@ import * as request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from '../src/app.module';
 import { PrismaService } from '../src/prisma/prisma.service';
+import { ArtistDto } from '../src/artist/dto/artist.dto';
+
+const dto: ArtistDto = {
+  name: 'test artist 01',
+  genre: 'pop',
+};
 
 describe('ArtistController (e2e)', () => {
   let app: INestApplication<App>;
@@ -18,12 +24,46 @@ describe('ArtistController (e2e)', () => {
     app.useGlobalPipes(new ValidationPipe());
 
     await app.init();
+
+    prisma = app.get(PrismaService);
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await prisma.artist.deleteMany();
+    await app.close();
+  });
+
+  it('POST /artists - should create artist', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/artists')
+      .send(dto)
+      .expect(201);
+
+    expect(response.body).toMatchObject(dto);
+    expect(response.body).toHaveProperty('id');
+  });
+
+  it('GET /artists/:id - should return 404 if artist not found', async () => {
+    await request(app.getHttpServer()).get('/artists/not-exist').expect(404);
+  });
+
+  it('GET /artists/:id - should return artist by id', async () => {
+    const created = await request(app.getHttpServer())
+      .post('/artists')
+      .send(dto)
+      .expect(201);
+
+    const artistId = created.body.id;
+
+    const response = await request(app.getHttpServer())
+      .get(`/artists/${artistId}`)
+      .expect(200);
+
+    expect(response.body).toMatchObject({
+      id: artistId,
+      name: dto.name,
+      genre: dto.genre,
+    });
+    expect(created.body).toHaveProperty('id');
   });
 });
