38f731ab448a489c0c2377523d9c7e4a8e898219
Aa <aa@example.com>
2025-05-04 10:34:35 +0300

    added tests

---
+++ src/artist/artist.service.spec.ts
@@ -0,0 +1,57 @@
+import { v4 as uuidv4 } from 'uuid';
+import { NotFoundException } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { Artist } from '../../generated/prisma';
+import { ArtistDto } from './dto/artist.dto';
+import { ArtistService } from './artist.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { ArtistController } from './artist.controller';
+
+const artistId = uuidv4();
+
+const artists: Artist[] = [
+  { id: artistId, name: 'test name 01', genre: 'pop' },
+  { id: uuidv4(), name: 'test name 02', genre: 'pop' },
+  { id: uuidv4(), name: 'test name 03', genre: 'pop' },
+];
+
+const artist: Artist = artists[0];
+
+const dto: ArtistDto = { name: artist.name, genre: artist.genre };
+
+const db = {
+  artist: {
+    findMany: () => jest.fn().mockResolvedValue(artists),
+    findUnique: () => jest.fn().mockResolvedValue(artist),
+    create: () => jest.fn().mockResolvedValue(artist),
+  },
+};
+
+describe('ArtistService', () => {
+  let service: ArtistService;
+  let prisma: PrismaService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        ArtistService,
+        {
+          provide: PrismaService,
+          useValue: db,
+        },
+      ],
+    }).compile();
+
+    prisma = module.get<PrismaService>(PrismaService);
+    service = module.get<ArtistService>(ArtistService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should return an array of artists', async () => {
+    const result = await service.findAll();
+    expect(result).toEqual(artists);
+  });
+});
