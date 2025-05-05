a6025dcfd4e2e757159faba0fbb922eddeb9ea89
Aa <aa@example.com>
2025-05-05 12:22:23 +0300

    added cron jobs

--- src/common/logger/logger.service.ts
+++ src/common/logger/logger.service.ts
@@ -1,8 +1,35 @@
-import { LoggerService, LogLevel } from '@nestjs/common';
-import { join } from 'path';
+import { ConsoleLogger, LoggerService, LogLevel } from '@nestjs/common';
+import { existsSync, mkdirSync, appendFileSync } from 'fs';
+import { dirname, join } from 'path';
 
 export class CustomLogger implements LoggerService {
   private readonly logFile = join(__dirname, `../../logs/custom.log`);
+  private readonly consoleLogger = new ConsoleLogger();
+
+  log(message: any, context: string) {
+    this.writeToFile('log', message, context);
+    this.consoleLogger.log(message, context);
+  }
+
+  error(message: any, context?: string, trace?: string) {
+    this.writeToFile('error', message, context, trace);
+    this.consoleLogger.error(message, trace, context);
+  }
+
+  warn(message: any, context: string) {
+    this.writeToFile('warn', message, context);
+    this.consoleLogger.warn(message, context);
+  }
+
+  debug(message: any, context: string) {
+    this.writeToFile('debug', message, context);
+    this.consoleLogger.debug(message, context);
+  }
+
+  verbose(message: any, context: string) {
+    this.writeToFile('verbose', message, context);
+    this.consoleLogger.verbose(message, context);
+  }
 
   private writeToFile(
     level: LogLevel,
@@ -11,6 +38,14 @@ export class CustomLogger implements LoggerService {
     trace?: string,
   ) {
     const time = new Date().toISOString();
-    const log = `[${time}] [${level}] ${context ? context : ''} ${message}  ${trace ? `\nTRACE: ${trace}` : ''}`;
+    const log = `[${time}] [${level}] ${context ? context : ''} ${message}  ${trace ? `\nTRACE: ${trace}` : ''}\n`;
+
+    const logDir = dirname(this.logFile);
+
+    if (!existsSync(logDir)) {
+      mkdirSync(logDir, { recursive: true });
+    }
+
+    appendFileSync(this.logFile, log);
   }
 }
