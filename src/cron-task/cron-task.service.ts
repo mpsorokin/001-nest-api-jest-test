c63d44a780c2d1851d0077ccce83c8506d5f29bf
Aa <aa@example.com>
2025-05-05 12:06:13 +0300

    added cron jobs

--- src/cron-task/cron-task.service.ts
+++ src/cron-task/cron-task.service.ts
@@ -1,18 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression, Interval, Timeout } from '@nestjs/schedule';
 
 @Injectable()
 export class CronTaskService {
-  /*@Cron(CronExpression.EVERY_10_SECONDS)
+  private readonly logger = new Logger('cron-task-service');
+
+  @Cron(CronExpression.EVERY_10_SECONDS)
   handleCron() {
-    console.log('Cron task runs every 10 seconds...');
-  }*/
-  /*@Interval(3 * 1000)
+    this.logger.log('Cron task runs every 10 seconds...');
+  }
+  @Interval(3 * 1000)
   handleInterval() {
-    console.log('Interval task runs every 3 seconds...');
-  }*/
-  /*@Timeout(5 * 1000)
+    this.logger.log('Interval task runs every 3 seconds...');
+  }
+  @Timeout(5 * 1000)
   handleTimeout() {
     console.log('Timeout every 5 seconds after start');
-  }*/
+  }
 }
